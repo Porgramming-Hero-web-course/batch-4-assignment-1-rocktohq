8a12cf057bf1353114be75c9b420ba749bbdfdeb
rocktohq <itzmonir@gmail.com>
2024-11-07 11:04:32 +0600

    Solution of Problem-8

--- Problem-8.ts
+++ Problem-8.ts
@@ -1,10 +1,3 @@
-function validateKeys<T>(obj: T, keys: (keyof T)[]) {
-  console.log(obj);
-  console.log(keys);
-
-  // 
-}
-
-// Sample Input:
-const person = { name: "Monir", age: 25, email: "monir@gmail.moc" };
-console.log(validateKeys(person, ["name", "age"]));
+const validateKeys = <T>(obj: T, keys: (keyof T)[]) => {
+  return keys.every((key) => obj[key]);
+};
