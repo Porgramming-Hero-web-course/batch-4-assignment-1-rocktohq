259e01afcfa18f0aa4886dea51a698cc32ea6e73
rocktohq <itzmonir@gmail.com>
2024-11-08 17:14:16 +0600

    Solution of Problem-4 Updated

--- Problem-4.ts
+++ Problem-4.ts
@@ -9,10 +9,14 @@ type Rectangle = {
   height: number;
 };
 
-const calculateShapeArea = (input: Circle | Rectangle) => {
+type Shape = Circle | Rectangle;
+
+const calculateShapeArea = (input: Shape): number | string => {
   if (input.shape === "circle") {
-    return (Math.PI * (input.radius * input.radius)).toFixed(2);
+    return parseFloat((Math.PI * (input.radius * input.radius)).toFixed(2));
   } else if (input.shape === "rectangle") {
     return input.width * input.height;
+  } else {
+    return "Invalid input!";
   }
 };
